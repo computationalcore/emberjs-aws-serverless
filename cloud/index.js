86d5917ab4fdea239c6067f17d06dea2ae66638b
Vin Busquet <computationalcore@gmail.com>
2019-05-04 20:12:30 -0300

    feat: Add main lambda handler and cloudformation template

---
+++ cloud/index.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const databaseManager = require('./databaseManager');
+const uuidv1 = require('uuid/v1');
+
+exports.userHandler = function(event, context, callback){
+
+    console.log(event, context);
+
+    switch (event.httpMethod) {
+			case 'DELETE':
+				deleteItem(event, callback);
+				break;
+			case 'GET':
+				getItem(event, callback);
+				break;
+			case 'POST':
+				saveItem(event, callback);
+				break;
+			case 'PUT':
+				updateItem(event, callback);
+				break;
+			default:
+				sendResponse(404, `Unsupported method "${event.httpMethod}"`, callback);
+	}
+};
+
+function saveItem(event, callback) {
+	const item = JSON.parse(event.body);
+
+	item.userId = uuidv1();
+
+	databaseManager.saveItem(item).then(response => {
+		sendResponse(200, item.userId, callback);
+	}, (reject) =>{
+		sendResponse(400, reject, callback);
+	});
+}
+
+function getItem(event, callback) {
+	if (event.pathParameters) {
+		const itemId = event.pathParameters.userId;
+		databaseManager.getItem(itemId).then(response => {
+			if(response)
+				sendResponse(200, response, callback);
+			else
+			sendResponse(404, "Please pass a valid userId", callback);
+	
+		},(reject) =>{
+			sendResponse(400, reject, callback);
+		});
+	}
+	else {
+		databaseManager.getItems().then(response => {
+			if(response)
+				sendResponse(200, response, callback);
+			else
+			sendResponse(404, "Please pass a valid userId", callback);
+	
+		},(reject) =>{
+			sendResponse(400, reject, callback);
+		});
+	}	
+}
+
+function deleteItem(event, callback) {
+	const itemId = event.pathParameters.userId;
+
+	databaseManager.deleteItem(itemId).then(response => {
+		sendResponse(200, 'DELETE ITEM', callback);
+	}, (reject) => {
+		sendResponse(400, reject, callback);
+	});
+}
+
+function updateItem(event, callback) {
+	const itemId = event.pathParameters.userId;
+
+	const body = JSON.parse(event.body);
+	
+	databaseManager.updateItem(itemId, body).then(response => {
+		sendResponse(200, response, callback);
+	}, (reject) => {
+		sendResponse(400, reject, callback);
+	});
+}
+
+function sendResponse(statusCode, message, callback) {
+	const response = {
+		statusCode: statusCode,
+		body: JSON.stringify(message)
+	};
+	callback(null, response);
+}
