6b48dcc326c2a0b8a103dfb8467aa2c649d90a42
Vin Busquet <computationalcore@gmail.com>
2019-05-04 18:41:55 -0300

    Add getItems to cloud databaseManager

--- cloud/databaseManager.js
+++ cloud/databaseManager.js
@@ -1,99 +1,123 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+
+if (process.env.AWS_SAM_LOCAL) {
+  AWS.config.update({
+    region: 'local',
+    endpoint: "http://dynamodb-local:8000",
+    maxRetries: 0
+  });
+}
+
 let dynamo = new AWS.DynamoDB.DocumentClient();
 
 const TABLE_NAME = 'UsersTable';
 
 module.exports.initializateDynamoClient = newDynamo => {
-	dynamo = newDynamo;
+  dynamo = newDynamo;
 };
 
 module.exports.saveItem = item => {
-	console.log('module.exports.saveItem', item)
-	const params = {
-		TableName: TABLE_NAME,
-		Item: item
-	};
+  const params = {
+    TableName: TABLE_NAME,
+    Item: item
+  };
 
-	return dynamo
-		.put(params)
-		.promise()
-		.then((result) => {
-			return item;
-		}, (error) => {
-			return error;
-		});
+  return dynamo
+    .put(params)
+    .promise()
+    .then((result) => {
+      return item;
+    }, (error) => {
+      return error;
+    });
 };
 
 module.exports.getItem = itemId => {
-	const params = {
-		Key: {
-			userId: itemId
-		},
-		TableName: TABLE_NAME
-	};
+  const params = {
+    Key: {
+      userId: itemId
+    },
+    TableName: TABLE_NAME
+  };
 
-	return dynamo
-		.get(params)
-		.promise()
-		.then((result) => {
-			return result.Item;
-		}, (error) => {
-			return error;
-		});
+  return dynamo
+    .get(params)
+    .promise()
+    .then((result) => {
+      return result.Item;
+    }, (error) => {
+      return error;
+    });
 };
 
-module.exports.deleteItem = itemId => {
-	const params = {
-		Key: {
-			userId: itemId
-		},
-		TableName: TABLE_NAME
-	};
+module.exports.getItems = () => {
+  const params = {
+    TableName: TABLE_NAME
+  };
 
-	return dynamo.delete(params).promise();
+  return dynamo
+    .scan(params)
+    .promise()
+    .then((result) => {
+      return result.Items;
+    }, (error) => {
+      return error;
+    });
+};
+
+
+module.exports.deleteItem = itemId => {
+  const params = {
+    Key: {
+      userId: itemId
+    },
+    TableName: TABLE_NAME
+  };
+
+  return dynamo.delete(params).promise();
 };
 
 module.exports.updateItem = (itemId, item) => {
-	
-	let vbl = "x";
-	let adder = "y";
-	let updateexp = 'set ';
-	let itemKeys =  Object.keys(item);
-	let expattvalues = {};
 
-	for (let i = 0; i < itemKeys.length; i++) {
-		vbl = vbl+adder;
+  let vbl = "x";
+  let adder = "y";
+  let updateexp = 'set ';
+  let itemKeys = Object.keys(item);
+  let expattvalues = {};
 
-		if((itemKeys.length-1)==i)
-			updateexp += itemKeys[i] + ' = :'+ vbl;
-		else
-			updateexp += itemKeys[i] + ' = :'+ vbl + ", ";
+  for (let i = 0; i < itemKeys.length; i++) {
+    vbl = vbl + adder;
 
-		expattvalues[":"+vbl] = item[itemKeys[i]];
-	}
+    if ((itemKeys.length - 1) == i)
+      updateexp += itemKeys[i] + ' = :' + vbl;
+    else
+      updateexp += itemKeys[i] + ' = :' + vbl + ", ";
 
-	console.log("update expression and expressionAttributeValues");
-	console.log(updateexp, expattvalues);
+    expattvalues[":" + vbl] = item[itemKeys[i]];
+  }
 
-	const params = {
-		TableName: TABLE_NAME,
-		Key: {
-			userId: itemId
-		},
-		ConditionExpression: 'attribute_exists(userId)',
-		UpdateExpression: updateexp,
-		ExpressionAttributeValues: expattvalues,
-		ReturnValues: 'ALL_NEW'
-	};
+  console.log("update expression and expressionAttributeValues");
+  console.log(updateexp, expattvalues);
 
-	return dynamo
-		.update(params)
-		.promise()
-		.then(response => {
-			return response.Attributes;
-		}, (error) => {
-			return error;
-		});
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      userId: itemId
+    },
+    ConditionExpression: 'attribute_exists(userId)',
+    UpdateExpression: updateexp,
+    ExpressionAttributeValues: expattvalues,
+    ReturnValues: 'ALL_NEW'
+  };
+
+  return dynamo
+    .update(params)
+    .promise()
+    .then(response => {
+      return response.Attributes;
+    }, (error) => {
+      return error;
+    });
 };
