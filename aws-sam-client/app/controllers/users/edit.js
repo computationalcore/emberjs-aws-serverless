44e35a3d5c6b575bc3118440ea98d05c28ef34e2
Vin Busquet <computationalcore@gmail.com>
2019-05-05 15:44:53 -0300

    feat: Add create/update loader

--- aws-sam-client/app/controllers/users/edit.js
+++ aws-sam-client/app/controllers/users/edit.js
@@ -1,30 +1,52 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    loading: false,
+    loadingText: '',
     actions: {
         editUser(id) {
             const self = this;
 
-            const firstName = this.get('model.firstName');
-            const lastName = this.get('model.lastName');
-            const address = this.get('model.address');
+            const firstName = this.get('model.firstName') ? this.get('model.firstName') : ' ';
+            const lastName = this.get('model.lastName') ? this.get('model.lastName') : ' ';
+            const address = this.get('model.address') ? this.get('model.address') : ' ';
 
             this.store.findRecord('user', id).then(function(user) {
                 user.set('firstName', firstName);
                 user.set('lastName', lastName);
                 user.set('address', address);
-
-                //Save to SAM App
-                user.save();
-
-                // Clear
+                
+                // Show loader
                 self.setProperties({
-                    firstName: '',
-                    lastName: '',
-                    address: ''
+                    loading: true,
+                    loadingText: `Saving ${firstName} ${lastName}...`
                 });
 
-                self.transitionToRoute('index');
+                //Save to SAM App
+                user.save()
+                .then(function(){
+                    // Clear
+                    self.setProperties({
+                        firstName: '',
+                        lastName: '',
+                        address: '',
+                        loading: false,
+                        loadingText: ''
+                    });
+
+                    self.transitionToRoute('index');
+                })
+                .catch(function(error){
+                    console.log(error);
+                    // Clear
+                    self.setProperties({
+                        loading: false,
+                        loadingText: ''
+                    });
+
+                    alert('Error while saving the data. Check the connection if the API server if up and running');
+                })
+                ;                
             });
         }
     }
--- aws-sam-client/app/controllers/users/new.js
+++ aws-sam-client/app/controllers/users/new.js
@@ -2,27 +2,50 @@ import Controller from '@ember/controller';
 
 
 export default Controller.extend({
+    loading: false,
+    loadingText: '',
     actions: {
       addUser() {
-        const firstName = this.get('firstName');
-        const lastName = this.get('lastName');
-        const address = this.get('address');
+        const self = this;
+
+        const firstName = this.get('firstName') ? this.get('firstName') : ' ';
+        const lastName = this.get('lastName') ? this.get('lastName') : ' ';
+        const address = this.get('address') ? this.get('address') : ' ';
         
         const newUser = this.store.createRecord('user', {
             firstName,
             lastName,
             address
         });
-        newUser.save();
 
-        // Clear
-        this.setProperties({
-            firstName: '',
-            lastName: '',
-            address: ''
+        // Show loader
+        self.setProperties({
+            loading: true,
+            loadingText: `Saving ${firstName} ${lastName}...`
         });
+        
+        newUser.save().then(function(){
+          // Clear
+          self.setProperties({
+              firstName: '',
+              lastName: '',
+              address: '',
+              loading: false,
+              loadingText: ''
+          });
 
-        this.transitionToRoute('index');
+          self.transitionToRoute('index');
+        })
+        .catch(function(error){
+          console.log(error);
+          // Clear
+          self.setProperties({
+              loading: false,
+              loadingText: ''
+          });
+
+          alert('Error while saving the data. Check the connection if the API server if up and running');
+        });
       },
     }
 });
