a196e797b050ece52cef89765da64c34066cc501
Vin Busquet <computationalcore@gmail.com>
2019-05-05 13:51:38 -0300

    feat: Add remove user logic to the client

--- aws-sam-client/app/controllers/index.js
+++ aws-sam-client/app/controllers/index.js
@@ -1,4 +1,26 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    // Used to control modal state
+    modal: false,
+    selectedUser: null,
+    actions: {
+        showModal(report) {
+            this.set('selectedUser', report);
+            this.set('modal', true);            
+        },
+        hideModal() {
+            this.set('modal', false);
+        },
+        deleteUser(id) {
+            const self = this;
+            this.store.findRecord('user', id).then(function(user) {
+                user.deleteRecord();
+                //Save to AWS SAM
+                user.save().then(function(){
+                    self.set('modal', false);
+                });
+            });
+        }
+    }
 });
