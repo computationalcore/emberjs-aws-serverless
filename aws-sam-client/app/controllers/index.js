e9a77c869df0089497bb459bc7c6db047a78b8ad
Vin Busquet <computationalcore@gmail.com>
2019-05-05 15:55:43 -0300

    feat: Add delete request loader

--- aws-sam-client/app/controllers/index.js
+++ aws-sam-client/app/controllers/index.js
@@ -1,6 +1,8 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    loading: false,
+    loadingText: '',
     // Used to control modal state
     modal: false,
     selectedUser: null,
@@ -14,12 +16,34 @@ export default Controller.extend({
         },
         deleteUser(id) {
             const self = this;
+
+            // Show loader
+            self.setProperties({
+                loading: true,
+                loadingText: `Removing user...`
+            });
+
             this.store.findRecord('user', id).then(function(user) {
                 user.deleteRecord();
                 //Save to AWS SAM
                 user.save().then(function(){
                     self.set('modal', false);
-                });
+                    // Clear
+                    self.setProperties({
+                        loading: false,
+                        loadingText: ''
+                    });
+                  })
+                  .catch(function(error){
+                    console.log(error);
+                    // Clear
+                    self.setProperties({
+                        loading: false,
+                        loadingText: ''
+                    });
+          
+                    alert('Error while saving the data. Check the connection if the API server if up and running');
+                  });
             });
         }
     }
